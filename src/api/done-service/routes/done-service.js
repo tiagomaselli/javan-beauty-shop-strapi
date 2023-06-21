8ca9557e87824e531d5863fef21c9a0b9e9e2c29
tiagomaselli <tiago.masellico@gmail.com>
2023-06-21 00:23:40 -0300

    excluido tabela service e alterado nome da tabela done-service para service

--- src/api/done-service/routes/done-service.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * done-service router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::done-service.done-service');
